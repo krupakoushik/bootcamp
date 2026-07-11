d978883b970df726811ab5ea90ad209d283228fd
krupa koushik <krupakoushikkona@gmail.com>
2026-07-11 19:48:17 +0530

    sensei and passes

--- frontend/src/components/bootcamp/Content.tsx
+++ frontend/src/components/bootcamp/Content.tsx
@@ -5,6 +5,7 @@ import Info from "@/components/camp-content/Info"
 import Venue from "@/components/camp-content/Venue"
 import Rules from "@/components/camp-content/Rules"
 import FAQ from "@/components/camp-content/FAQ"
+import Passes from "@/components/camp-content/Passes"
 
 
 
@@ -80,47 +81,7 @@ export default function Content() {
                     <div className="h-px w-20 bg-gold-soft/30" />
                 </div>
 
-                {/* ================= PASS ================= */}
-
-                <section className="mt-5">
-
-                    <div className="grid lg:grid-cols-2 gap-5 rounded-3xl border border-gold-soft/50 bg-white/3 backdrop-blur-xl p-10 mt-5">
-                        <div>
-                            <p className="font-anton tracking-[0.8em] text-gold-soft uppercase">
-                                Beginner Bootcamp Pass
-                            </p>
-                            <div>
-
-                                <h2 className="font-bebas text-9xl text-cream">
-
-                                    ₹3000
-
-                                </h2>
-
-                                <p className="text-white/60 mt-3 text-cream">
-
-                                    One registration.
-                                    Two unforgettable days.
-
-                                </p>
-                            </div>
-                        </div>
-                        <div>
-                            <ul className="space-y-5 text-cream">
-                                <li><span className="text-gold-soft"> 𒉽 </span> Two Full Days of Kendo Training</li>
-                                <li><span className="text-gold-soft"> 𒉽 </span> Shinai Rental Included</li>
-                                <li><span className="text-gold-soft"> 𒉽 </span> Vegetarian Lunch</li>
-                                <li><span className="text-gold-soft"> 𒉽 </span> Completion Certificate</li>
-                                <li><span className="text-gold-soft"> 𒉽 </span> CKC Membership Benefits</li>
-                            </ul>
-
-                        </div>
-
-                    </div>
-
-                </section>
-
-
+                <Passes />
 
 
             </div>
