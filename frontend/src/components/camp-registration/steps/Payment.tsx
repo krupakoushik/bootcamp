36183b9a7696ed42155c44a7d99a9f4f1272d60d
krupa koushik <krupakoushikkona@gmail.com>
2026-07-16 18:02:39 +0530

    changes to payment

--- frontend/src/components/camp-registration/steps/Payment.tsx
+++ frontend/src/components/camp-registration/steps/Payment.tsx
@@ -220,6 +220,7 @@ export default function Payment({
                         </div>
                     <button
                         onClick={() => {
+                            console.log(upiLink);
                             window.open(upiLink, "_self");
                         }}
                         disabled={loading}
